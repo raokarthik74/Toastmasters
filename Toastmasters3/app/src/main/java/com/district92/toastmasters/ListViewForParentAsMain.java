3c8fc1383f1fa426a789ffa12e24e5d8eb2bb3fc
Karthik Rao <raokarthik74@gmail.com>
2015-09-16 14:03:21 +0530

    deleted unwanted files

--- Toastmasters3/app/src/main/java/com/district92/toastmasters/ListViewForParentAsMain.java
+++ Toastmasters3/app/src/main/java/com/district92/toastmasters/ListViewForParentAsMain.java
@@ -1,23 +1,22 @@
 package com.district92.toastmasters;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class clubDetailActivity extends ActionBarActivity {
+public class ListViewForParentAsMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_club_detail);
+        setContentView(R.layout.activity_list_view_for_parent_as_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_club_detail, menu);
+        getMenuInflater().inflate(R.menu.menu_list_view_for_parent_as_main, menu);
         return true;
     }
 
@@ -29,9 +28,9 @@ public class clubDetailActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
