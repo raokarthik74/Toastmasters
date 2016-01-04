12f8d8af8a818c16e559e7b62cd3ee65ee16d5bc
Karthik Rao <raokarthik74@gmail.com>
2016-01-03 19:35:29 -0800

    picture changes and parse

--- ToastDirector/app/src/main/java/com/karthikravindrarao/ToastDirector/util/ParseNotification.java
+++ ToastDirector/app/src/main/java/com/karthikravindrarao/ToastDirector/util/ParseNotification.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseInstallation;
 import com.parse.ParsePush;
+import com.parse.PushService;
 
 /**
  * Created by Karthik on 9/22/15.
@@ -12,7 +13,7 @@ import com.parse.ParsePush;
 public class ParseNotification extends Application{
     public void onCreate() {
         super.onCreate();
-        Parse.initialize(this, "ljsPLiJkLxXc3pQkJ0yhTw5ZO7BWdMJ5WpCpJ8Tr", "eex0SpUx13oTpqtQ8wZvxpdMnoeaO3Ed2zU0nXXW");
+        Parse.initialize(this, "8TbpDkwFUDXKSsgA9CWsp5aN8d1uFEHc0WyBR8Aw", "9c0bLnaUaGH4aOIU7VjOKT3cZmN9ssfvI7hnz19y");
         ParsePush.subscribeInBackground("DISTRICT-92");
         ParseInstallation.getCurrentInstallation().saveInBackground();
     }
